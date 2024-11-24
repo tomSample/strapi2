7e26389202918b589f06385c1671bdb71696a645
Thomas Grandjean <thomasgrandjean45@gmail.com>
2024-11-24 14:52:43 +0100

    backend functionnal with api/home?populate=*

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => {
       connection: {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
+        database: env('DATABASE_NAME', 'strapi_portfolio_db'),
+        user: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
